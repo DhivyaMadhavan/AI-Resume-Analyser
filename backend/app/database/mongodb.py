2d82c5fae01aa8efca9db96cef8ad744f9ba6c0f
DhivyaMadhavan <139535300+DhivyaMadhavan@users.noreply.github.com>
2026-07-13 02:05:19 +0530

    Update mongodb.py

--- backend/app/database/mongodb.py
+++ backend/app/database/mongodb.py
@@ -1,20 +1,10 @@
 from pymongo import MongoClient
 from app.config import settings
-import time
-
-
-start = time.perf_counter()
 
 client = MongoClient(
     settings.MONGO_URI,
     serverSelectionTimeoutMS=5000
 )
-
-print(
-    "Mongo client creation:",
-    time.perf_counter() - start
-)
-
 
 start = time.perf_counter()
 
@@ -22,8 +12,3 @@ db = client[settings.DATABASE_NAME]
 
 resume_collection = db["resume_analysis"]
 matching_collection = db["jd_matching"]
-
-print(
-    "Mongo collection setup:",
-    time.perf_counter() - start
-)
