eff5e2db75c91eab639dfb5ecff3ec60fd624853
DhivyaMadhavan <139535300+DhivyaMadhavan@users.noreply.github.com>
2026-07-12 18:32:46 +0530

    Update redis.py

--- backend/app/database/redis.py
+++ backend/app/database/redis.py
@@ -6,6 +6,8 @@ redis_client = redis.Redis(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
     db=settings.REDIS_DB,
+    username=settings.REDIS_USER,
+    password=settings.REDIS_PASSWORD,
     decode_responses=True,
 
     socket_connect_timeout=0.1,
