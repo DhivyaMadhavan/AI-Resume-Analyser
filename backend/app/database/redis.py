5780438fda7ac84f93420a0a12517b440b496c28
DhivyaMadhavan <139535300+DhivyaMadhavan@users.noreply.github.com>
2026-07-12 19:01:03 +0530

    Update redis.py

--- backend/app/database/redis.py
+++ backend/app/database/redis.py
@@ -3,13 +3,18 @@ from app.config import settings
 
 # Create a Redis client
 redis_client = redis.Redis(
+    redis_client = redis.Redis(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
-    db=settings.REDIS_DB,
     username=settings.REDIS_USER,
     password=settings.REDIS_PASSWORD,
+    db=settings.REDIS_DB,
+
+    ssl=True,
+
     decode_responses=True,
 
-    socket_connect_timeout=0.1,
-    socket_timeout=0.1
+    socket_connect_timeout=5,
+    socket_timeout=5
+)
 )
